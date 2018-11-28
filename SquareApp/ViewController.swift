98eee0cb6acd8cc3c05c76fa2f780460f73e7181
Stanislau Sakharchuk <sahar0217@yandex.ru>
2018-11-28 20:21:54 +0200

    Initial Commit

---
+++ SquareApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SquareApp
+//
+//  Created by computer_new on 11/28/18.
+//  Copyright © 2018 Square. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
