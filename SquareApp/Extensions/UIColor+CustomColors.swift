b45f9f1db4ed1f5d47f2883915d460cb3de08f0b
Stanislau Sakharchuk <sahar0217@yandex.ru>
2018-11-28 23:32:50 +0200

    adds auth module

---
+++ SquareApp/Extensions/UIColor+CustomColors.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+CustomColors.swift
+//  SquareApp
+//
+//  Created by computer_new on 11/28/18.
+//  Copyright © 2018 Square. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    enum Custom {
+        static let accent = UIColor(red:0.84, green:0.05, blue:0.55, alpha:1)
+    }
+}
---
+++ SquareApp/Modules/Auth/ConnectNetworks/ConnectNetworksViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ConnectNetworksViewController.swift
+//  SquareApp
+//
+//  Created by computer_new on 11/28/18.
+//  Copyright © 2018 Square. All rights reserved.
+//
+
+import UIKit
+
+class ConnectNetworksViewController: UIViewController {
+
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var createYourProfileLabel: UILabel!
+    @IBOutlet weak var connectDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var facebookButton: UIButton!
+    @IBOutlet weak var googleButton: UIButton!
+    @IBOutlet weak var tripadvisorButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
