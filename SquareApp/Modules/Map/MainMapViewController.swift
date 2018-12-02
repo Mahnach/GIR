7b4d746b9bf8da7dea057938aced4caaf61e7441
Igor Kasyanenko <dekstersniper@gmail.com>
2018-12-02 18:36:30 +0200

    adds mapbox

--- SquareApp/Modules/Map/MainMapViewController.swift
+++ SquareApp/Modules/Map/MainMapViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
+import Mapbox
 
 class MainMapViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MGLMapView!
+    
+    @IBAction func moveToCurrentLocationTouched(_ sender: UIButton) {
+        moveToCurrentLocation()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         let search = UISearchController(searchResultsController: nil)
         navigationItem.titleView = search.searchBar
         tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "tabbar_map"), tag: 0)
         tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        
+        
+        mapView.showsUserLocation = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.moveToCurrentLocation()
+        }
     }
     
+    
+    func moveToCurrentLocation() {
+        if let userLocation = self.mapView.userLocation {
+            
+            let camera = MGLMapCamera(lookingAtCenter: userLocation.coordinate, fromDistance: 4000, pitch: 0, heading: 0)
+            self.mapView.fly(to: camera, completionHandler: nil)
+        }
+    }
 
     /*
     // MARK: - Navigation
